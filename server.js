9f0278cb3089cd6cb86c1c369eea22b0bc7667c8
Devyn <heuers.hues@gmail.com>
2022-09-04 14:51:28 +0200

    adding backend of smartbrain

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    port: '5432',
+    password: "2580",
+    database: "smartbrain",
+  },
+});
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {res.send(db.users);});
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", register.handleRegister(db, bcrypt))
+app.get("/profile/:id", (req, res) => {profile.handleProfileGet(req, res, db)});
+app.put("/image", (req, res) => { image.handleImage(req, res, db)});
+app.post("/imageurl", (req, res) => { image.handleApiCall(req, res)});
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
